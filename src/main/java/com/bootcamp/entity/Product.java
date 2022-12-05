c54e4cb0fa0091a3293d6b5d0d49034efb74ebe9
DevCescNttData <cmendocu@emeal.nttdata.com>
2022-12-05 10:25:31 -0500

    update class product

--- src/main/java/com/bootcamp/entity/Product.java
+++ src/main/java/com/bootcamp/entity/Product.java
@@ -1,7 +1,6 @@
 package com.bootcamp.entity;
 
 import lombok.*;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
